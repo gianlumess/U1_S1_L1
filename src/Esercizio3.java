2d8c0e97fcfcd1c12177519382a2fbd54b8152c0
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-22 16:57:38 +0200

    esercizio 3 finito

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -1,5 +1,8 @@
 public class Esercizio3 {
     public static void main(String[] args) {
+
+        System.out.println("-----------------------------METODO PERIMETRO-RETTANGOLO-------------------------");
+
         double lato1 = 5.2;
         double lato2 = 3.7;
         double perimetro = perimetroRettangolo(lato1, lato2);
@@ -12,6 +15,17 @@ public class Esercizio3 {
 
         System.out.println("Il numero " + numeroPari + " è " + pariDispari(numeroPari));
         System.out.println("Il numero " + numeroDispari + " è " + pariDispari(numeroDispari));
+
+        System.out.println("-----------------------------METODO AREA-TRIANGOLO-------------------------");
+
+        double latoTriangolo1 = 3.0;
+        double latoTriangolo2 = 4.0;
+        double latoTriangolo3 = 5.0;
+
+        double area = areaTriangolo(latoTriangolo1, latoTriangolo2, latoTriangolo3);
+
+        System.out.println("L'area del triangolo è: " + area);
+
     }
 
     //metodo che calcola il perimetro di un rettangolo inserendo i 2 lati
@@ -27,5 +41,16 @@ public class Esercizio3 {
         } else {
             return 1; // Restituisce 1 se il numero è dispari
         }
+    }
+
+    public static double areaTriangolo(double lato1, double lato2, double lato3) {
+        // Calcola il semiperimetro
+        double s = (lato1 + lato2 + lato3) / 2.0;
+
+        // Calcola l'area usando la formula di Erone
+        double area = Math.sqrt(s * (s - lato1) * (s - lato2) * (s - lato3));
+
+        // Restituisce l'area
+        return area;
     }
 }
